d1d40d52fad1dbb69757a4224bcc946878a90ff4
Ars Web <128975695+arswebid@users.noreply.github.com>
2023-04-01 05:39:13 +0700

    Update simple-login-model.js
    In this code, we import and use the following packages:
    express: A web framework for Node.js
    passport: An authentication middleware for Node.js
    passport-local: A Passport strategy for authenticating with a username and password
    bcrypt: A library for hashing passwords
    We also define a mock user database with two users.
    
    We create a new Express app and add the following middleware:
    express.urlencoded: Parses incoming requests with urlencoded payloads
    express.json: Parses incoming requests with JSON payloads
    passport.initialize: Initializes Passport authentication middleware
    We define a new local strategy for Passport using passport.use:
    The LocalStrategy constructor takes a callback function that is used to verify a user's credentials.
    The callback function takes a username, password, and done callback function.
    If the username is not found in the user database, the done function is called with false and an error message.
    If the username is found, the stored password hash is compared to the provided password using bcrypt.compare.

--- simple-login-model.js
+++ simple-login-model.js
@@ -67,4 +67,4 @@ app.get("/profile", (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server started on port 3000"));
+app.listen(3000, () => console.log("Server started on port 3000")); 
